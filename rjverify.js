3c2d910f98335150a3ce3a2bc2c9f319b491bef6
linzsx007001 <152753792+linzsx007001@users.noreply.github.com>
2025-03-19 21:10:53 +0800

    Update rjverify.js

--- rjverify.js
+++ rjverify.js
@@ -1,53 +1,74 @@
-import explicitRenderHtml from './explicit.html';
-import implicitRenderHtml from './implicit.html';
-
-// This is the demo secret key. In prod, we recommend you store
-// your secret key(s) safely.
-const SECRET_KEY = '0x4AAAAAABBmDL5EZbF2AG4R;
+// This is the demo secret key. In production, we recommend
+// you store your secret key(s) safely.
+const SECRET_KEY = "0x4AAAAAABBmDL5EZbF2AG4R";
 
 async function handlePost(request) {
-    const body = await request.formData();
-    // Turnstile injects a token in "cf-turnstile-response".
-    const token = body.get('cf-turnstile-response');
-    const ip = request.headers.get('CF-Connecting-IP');
+  const body = await request.formData();
+  // Turnstile injects a token in "cf-turnstile-response".
+  const token = body.get("cf-turnstile-response");
+  const ip = request.headers.get("CF-Connecting-IP");
 
-    // Validate the token by calling the "/siteverify" API.
-    let formData = new FormData();
-    formData.append('secret', SECRET_KEY);
-    formData.append('response', token);
-    formData.append('remoteip', ip);
+  // Validate the token by calling the
+  // "/siteverify" API endpoint.
+  let formData = new FormData();
+  formData.append("secret", SECRET_KEY);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
 
-    const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-        body: formData,
-        method: 'POST',
-    });
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+  const result = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
 
-    const outcome = await result.json();
-    if (!outcome.success) {
-        return new Response('The provided Turnstile token was not valid! \n' + JSON.stringify(outcome));
-    }
-    // The Turnstile token was successfuly validated. Proceed with your application logic.
-    // Validate login, redirect user, etc.
-    // For this demo, we just echo the "/siteverify" response:
-    return new Response('Turnstile token successfuly validated. \n' + JSON.stringify(outcome));
+  const outcome = await result.json();
+  if (outcome.success) {
+    // ...
+  }
 }
 
-export default {
-    async fetch(request) {
-        if (request.method === 'POST') {
-            return await handlePost(request);
-        }
+url encoded
+JSON
+Example using idempotency functionality
+// This is the demo secret key. In production, we recommend
+// you store your secret key(s) safely.
+const SECRET_KEY = "0x4AAAAAABBmDL5EZbF2AG4R";
 
-        const url = new URL(request.url);
-        let body = implicitRenderHtml;
-        if (url.pathname === '/explicit') {
-            body = explicitRenderHtml;
-        }
-        
-        return new Response(body, {
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
-    },
-};
+async function handlePost(request) {
+  const body = await request.formData();
+  // Turnstile injects a token in "cf-turnstile-response".
+  const token = body.get("cf-turnstile-response");
+  const ip = request.headers.get("CF-Connecting-IP");
+
+  // Validate the token by calling the
+  // "/siteverify" API endpoint.
+  let formData = new FormData();
+  formData.append("secret", SECRET_KEY);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
+  const idempotencyKey = crypto.randomUUID();
+  formData.append("idempotency_key", idempotencyKey);
+
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+  const firstResult = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+  const firstOutcome = await firstResult.json();
+  if (firstOutcome.success) {
+    
+  }
+
+  // A subsequent validation request to the "/siteverify"
+  // API endpoint for the same token as before, providing
+  // the associated idempotency key as well.
+  const subsequentResult = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+
+  const subsequentOutcome = await subsequentResult.json();
+  if (subsequentOutcome.success) {
+    // ...
+  }
+}
